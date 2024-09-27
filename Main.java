756676751ed6bd161839c5f2110da41334e5a29d
katarinaVanish <143878109+katarinaVanish@users.noreply.github.com>
2024-09-27 20:26:36 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,5 +10,11 @@ public class Main {
 
         Task2 task2 = new Task2();
         task2.execute();
+
+        Task3 task3 = new Task3();
+        task3.execute();
+
+        Task4 task4 = new Task4();
+        task4.execute();
     }
 }
