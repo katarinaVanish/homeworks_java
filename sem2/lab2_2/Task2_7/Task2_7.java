f3b8c8e883c614d96fba9665178249b8224a222c
Karina <143878109+katarinaVanish@users.noreply.github.com>
2025-03-15 10:40:19 +0400

    Update Task2_7.java

--- sem2/lab2_2/Task2_7/Task2_7.java
+++ sem2/lab2_2/Task2_7/Task2_7.java
@@ -1,5 +1,14 @@
 package org.knit.sem2.lab2_2.Task2_7;
 
+@TaskDescription(7, "Производитель-Потребитель с ограничением");
+/*
+Описание:
+- Производитель создает товары (максимум 5).
+- Потребитель забирает товары.
+- Если товаров нет, потребитель ждет (wait()).
+- Если товаров максимум, производитель ждет (wait()).
+*/
+
 public class Task2_7 {
     public static void main(String[] args) {
         Warehouse warehouse = new Warehouse();
