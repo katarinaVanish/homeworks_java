fd403622a814f78ef7523de5038fa80898dff23f
Karina <143878109+katarinaVanish@users.noreply.github.com>
2025-03-15 10:36:13 +0400

    Update Task2_4.java

--- sem2/lab2_2/Task2_4/Task2_4.java
+++ sem2/lab2_2/Task2_4/Task2_4.java
@@ -1,5 +1,11 @@
 package org.knit.sem2.lab2_2.Task2_4;
 
+@TaskDescription(4, "Автозаправочная станция");
+/*
+На автозаправочной станции только 2 колонки. Если все заняты, машины ждут в очереди.
+Когда колонка освобождается, следующий автомобиль из очереди начинает заправку.
+*/
+
 public class Task2_4 {
     public static void main(String[] args) {
         GasStation station = new GasStation();
